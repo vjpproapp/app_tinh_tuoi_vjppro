f88a31fd8773abc2a2ffaf5dc68ce42bab5484fa
vjpproapp <134807113+vjpproapp@users.noreply.github.com>
2023-05-28 02:40:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function calcAge() {
     document.getElementById("getAge").innerHTML = `<br><span>Tuổi của các bro là: ${tuoi}  ( ಠ ͜ʖಠ) </span>
     <br><br>
     <img style='width:250px' src="./IQ.png"/>`;
-    document.getElementById("getAge").style.display = "none";
+    document.getElementById("btn").style.display = "none";
+
 }
