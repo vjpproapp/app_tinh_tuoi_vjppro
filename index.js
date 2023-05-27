765753ffacaa2553e77b4938b3a1f1eb8c4b0b47
vjpproapp <134807113+vjpproapp@users.noreply.github.com>
2023-05-28 02:13:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function calcAge() {
+    let tuoi = document.getElementById('tuoi').value
+
+    document.getElementById("getAge").innerHTML = `<br><span>Tuổi của các bro là: ${tuoi}  ( ಠ ͜ʖಠ) </span>
+    <br><br>
+    <img style='width:250px' src="./IQ.png"/>`  
+}
